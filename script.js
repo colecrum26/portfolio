4c869439d5e4d02eb53db91baf7c168885165262
colecrum26 <cwcrumrine@gmail.com>
2023-03-03 17:13:19 -0600

    script running

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-var button = 
-var box;
+var button = document.querySelector('button');
+var box = document.getElementById('clickMe');
 
+function changeColor(){
+    box.style.background = 'blue';
+}
