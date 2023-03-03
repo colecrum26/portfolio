c7264879bfb5ee3ace676e180f29a88006fe4699
colecrum26 <cwcrumrine@gmail.com>
2023-03-03 17:22:27 -0600

    added red/blue toggle

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ var button = document.querySelector('button');
 var box = document.getElementById('clickMe');
 
 function changeColor(){
-    box.style.background = 'blue';
+    if(box.style.background == 'red'){
+        box.style.background = 'blue';
+    }else{
+    box.style.background = 'red';
+    }
 }
+    
